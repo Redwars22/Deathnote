de5f7d012fa3908c47fbfb030736cae281710241
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-09-13 14:37:13 -0300

    diálogos de salvar e abrir arquivos, contador de palavras

--- Deathnote/MainForm.Designer.vb
+++ Deathnote/MainForm.Designer.vb
@@ -22,57 +22,155 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
         Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(MainForm))
         MenuStrip1 = New MenuStrip()
         ArquivoToolStripMenuItem = New ToolStripMenuItem()
+        AbrirToolStripMenuItem = New ToolStripMenuItem()
+        SalvarToolStripMenuItem = New ToolStripMenuItem()
         EditarToolStripMenuItem = New ToolStripMenuItem()
+        FonteToolStripMenuItem = New ToolStripMenuItem()
+        CorDoTextoToolStripMenuItem = New ToolStripMenuItem()
         ExibirToolStripMenuItem = New ToolStripMenuItem()
+        TemaToolStripMenuItem = New ToolStripMenuItem()
+        ClaroToolStripMenuItem = New ToolStripMenuItem()
+        EscuroToolStripMenuItem = New ToolStripMenuItem()
         InserirToolStripMenuItem = New ToolStripMenuItem()
+        MarkdownToolStripMenuItem = New ToolStripMenuItem()
+        TítuloToolStripMenuItem = New ToolStripMenuItem()
+        HTMLToolStripMenuItem = New ToolStripMenuItem()
+        ParágrafoToolStripMenuItem = New ToolStripMenuItem()
         AjudaToolStripMenuItem = New ToolStripMenuItem()
         DeathNoteEditor = New RichTextBox()
+        OpenFileDialog1 = New OpenFileDialog()
+        SaveFileDialog1 = New SaveFileDialog()
+        ColorDialog1 = New ColorDialog()
+        FontDialog1 = New FontDialog()
+        StatusStrip1 = New StatusStrip()
+        OpenFileName = New ToolStripStatusLabel()
+        WordCounter = New ToolStripStatusLabel()
+        NotifyIcon1 = New NotifyIcon(components)
+        SobreODeathnoteToolStripMenuItem = New ToolStripMenuItem()
+        NotasDeVersãoToolStripMenuItem = New ToolStripMenuItem()
         MenuStrip1.SuspendLayout()
+        StatusStrip1.SuspendLayout()
         SuspendLayout()
         ' 
         ' MenuStrip1
         ' 
-        MenuStrip1.BackColor = SystemColors.WindowFrame
+        MenuStrip1.BackColor = SystemColors.ControlDarkDark
         MenuStrip1.Items.AddRange(New ToolStripItem() {ArquivoToolStripMenuItem, EditarToolStripMenuItem, ExibirToolStripMenuItem, InserirToolStripMenuItem, AjudaToolStripMenuItem})
         MenuStrip1.Location = New Point(0, 0)
         MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.RenderMode = ToolStripRenderMode.System
         MenuStrip1.Size = New Size(696, 24)
         MenuStrip1.TabIndex = 0
         MenuStrip1.Text = "MenuStrip1"
         ' 
         ' ArquivoToolStripMenuItem
         ' 
+        ArquivoToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {AbrirToolStripMenuItem, SalvarToolStripMenuItem})
         ArquivoToolStripMenuItem.ForeColor = Color.White
         ArquivoToolStripMenuItem.Name = "ArquivoToolStripMenuItem"
         ArquivoToolStripMenuItem.Size = New Size(61, 20)
         ArquivoToolStripMenuItem.Text = "Arquivo"
         ' 
+        ' AbrirToolStripMenuItem
+        ' 
+        AbrirToolStripMenuItem.Name = "AbrirToolStripMenuItem"
+        AbrirToolStripMenuItem.Size = New Size(105, 22)
+        AbrirToolStripMenuItem.Text = "Abrir"
+        ' 
+        ' SalvarToolStripMenuItem
+        ' 
+        SalvarToolStripMenuItem.Name = "SalvarToolStripMenuItem"
+        SalvarToolStripMenuItem.Size = New Size(105, 22)
+        SalvarToolStripMenuItem.Text = "Salvar"
+        ' 
         ' EditarToolStripMenuItem
         ' 
+        EditarToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {FonteToolStripMenuItem, CorDoTextoToolStripMenuItem})
         EditarToolStripMenuItem.ForeColor = Color.White
         EditarToolStripMenuItem.Name = "EditarToolStripMenuItem"
         EditarToolStripMenuItem.Size = New Size(49, 20)
         EditarToolStripMenuItem.Text = "Editar"
         ' 
+        ' FonteToolStripMenuItem
+        ' 
+        FonteToolStripMenuItem.Name = "FonteToolStripMenuItem"
+        FonteToolStripMenuItem.Size = New Size(141, 22)
+        FonteToolStripMenuItem.Text = "Fonte"
+        ' 
+        ' CorDoTextoToolStripMenuItem
+        ' 
+        CorDoTextoToolStripMenuItem.Name = "CorDoTextoToolStripMenuItem"
+        CorDoTextoToolStripMenuItem.Size = New Size(141, 22)
+        CorDoTextoToolStripMenuItem.Text = "Cor do Texto"
+        ' 
         ' ExibirToolStripMenuItem
         ' 
+        ExibirToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {TemaToolStripMenuItem})
         ExibirToolStripMenuItem.ForeColor = Color.White
         ExibirToolStripMenuItem.Name = "ExibirToolStripMenuItem"
         ExibirToolStripMenuItem.Size = New Size(48, 20)
         ExibirToolStripMenuItem.Text = "Exibir"
         ' 
+        ' TemaToolStripMenuItem
+        ' 
+        TemaToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ClaroToolStripMenuItem, EscuroToolStripMenuItem})
+        TemaToolStripMenuItem.Name = "TemaToolStripMenuItem"
+        TemaToolStripMenuItem.Size = New Size(102, 22)
+        TemaToolStripMenuItem.Text = "Tema"
+        ' 
+        ' ClaroToolStripMenuItem
+        ' 
+        ClaroToolStripMenuItem.Name = "ClaroToolStripMenuItem"
+        ClaroToolStripMenuItem.Size = New Size(109, 22)
+        ClaroToolStripMenuItem.Text = "Claro"
+        ' 
+        ' EscuroToolStripMenuItem
+        ' 
+        EscuroToolStripMenuItem.Name = "EscuroToolStripMenuItem"
+        EscuroToolStripMenuItem.Size = New Size(109, 22)
+        EscuroToolStripMenuItem.Text = "Escuro"
+        ' 
         ' InserirToolStripMenuItem
         ' 
+        InserirToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {MarkdownToolStripMenuItem, HTMLToolStripMenuItem})
         InserirToolStripMenuItem.ForeColor = Color.White
         InserirToolStripMenuItem.Name = "InserirToolStripMenuItem"
         InserirToolStripMenuItem.Size = New Size(51, 20)
         InserirToolStripMenuItem.Text = "Inserir"
         ' 
+        ' MarkdownToolStripMenuItem
+        ' 
+        MarkdownToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {TítuloToolStripMenuItem})
+        MarkdownToolStripMenuItem.Name = "MarkdownToolStripMenuItem"
+        MarkdownToolStripMenuItem.Size = New Size(131, 22)
+        MarkdownToolStripMenuItem.Text = "Markdown"
+        ' 
+        ' TítuloToolStripMenuItem
+        ' 
+        TítuloToolStripMenuItem.Name = "TítuloToolStripMenuItem"
+        TítuloToolStripMenuItem.Size = New Size(104, 22)
+        TítuloToolStripMenuItem.Text = "Título"
+        ' 
+        ' HTMLToolStripMenuItem
+        ' 
+        HTMLToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ParágrafoToolStripMenuItem})
+        HTMLToolStripMenuItem.Name = "HTMLToolStripMenuItem"
+        HTMLToolStripMenuItem.Size = New Size(131, 22)
+        HTMLToolStripMenuItem.Text = "HTML"
+        ' 
+        ' ParágrafoToolStripMenuItem
+        ' 
+        ParágrafoToolStripMenuItem.Name = "ParágrafoToolStripMenuItem"
+        ParágrafoToolStripMenuItem.Size = New Size(125, 22)
+        ParágrafoToolStripMenuItem.Text = "Parágrafo"
+        ' 
         ' AjudaToolStripMenuItem
         ' 
+        AjudaToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {SobreODeathnoteToolStripMenuItem, NotasDeVersãoToolStripMenuItem})
         AjudaToolStripMenuItem.ForeColor = Color.White
         AjudaToolStripMenuItem.Name = "AjudaToolStripMenuItem"
         AjudaToolStripMenuItem.Size = New Size(50, 20)
@@ -81,6 +179,7 @@ Partial Class MainForm
         ' DeathNoteEditor
         ' 
         DeathNoteEditor.BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        DeathNoteEditor.BorderStyle = BorderStyle.None
         DeathNoteEditor.Dock = DockStyle.Fill
         DeathNoteEditor.Font = New Font("Liberation Mono", 12F, FontStyle.Regular, GraphicsUnit.Point)
         DeathNoteEditor.ForeColor = Color.White
@@ -90,12 +189,62 @@ Partial Class MainForm
         DeathNoteEditor.TabIndex = 1
         DeathNoteEditor.Text = ""
         ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        ' 
+        ' SaveFileDialog1
+        ' 
+        ' 
+        ' StatusStrip1
+        ' 
+        StatusStrip1.BackColor = SystemColors.ControlDarkDark
+        StatusStrip1.Items.AddRange(New ToolStripItem() {OpenFileName, WordCounter})
+        StatusStrip1.Location = New Point(0, 428)
+        StatusStrip1.Name = "StatusStrip1"
+        StatusStrip1.RenderMode = ToolStripRenderMode.Professional
+        StatusStrip1.Size = New Size(696, 22)
+        StatusStrip1.TabIndex = 2
+        StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' OpenFileName
+        ' 
+        OpenFileName.ForeColor = Color.DodgerBlue
+        OpenFileName.Name = "OpenFileName"
+        OpenFileName.Size = New Size(138, 17)
+        OpenFileName.Text = "Nenhum Arquivo Aberto"
+        ' 
+        ' WordCounter
+        ' 
+        WordCounter.ForeColor = Color.Silver
+        WordCounter.Name = "WordCounter"
+        WordCounter.Size = New Size(59, 17)
+        WordCounter.Text = "0 palavras"
+        ' 
+        ' NotifyIcon1
+        ' 
+        NotifyIcon1.Text = "NotifyIcon1"
+        NotifyIcon1.Visible = True
+        ' 
+        ' SobreODeathnoteToolStripMenuItem
+        ' 
+        SobreODeathnoteToolStripMenuItem.Name = "SobreODeathnoteToolStripMenuItem"
+        SobreODeathnoteToolStripMenuItem.Size = New Size(180, 22)
+        SobreODeathnoteToolStripMenuItem.Text = "Sobre o Deathnote"
+        ' 
+        ' NotasDeVersãoToolStripMenuItem
+        ' 
+        NotasDeVersãoToolStripMenuItem.Name = "NotasDeVersãoToolStripMenuItem"
+        NotasDeVersãoToolStripMenuItem.Size = New Size(180, 22)
+        NotasDeVersãoToolStripMenuItem.Text = "Notas de Versão"
+        ' 
         ' MainForm
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = SystemColors.ActiveCaptionText
         ClientSize = New Size(696, 450)
+        Controls.Add(StatusStrip1)
         Controls.Add(DeathNoteEditor)
         Controls.Add(MenuStrip1)
         Icon = CType(resources.GetObject("$this.Icon"), Icon)
@@ -107,6 +256,8 @@ Partial Class MainForm
         WindowState = FormWindowState.Maximized
         MenuStrip1.ResumeLayout(False)
         MenuStrip1.PerformLayout()
+        StatusStrip1.ResumeLayout(False)
+        StatusStrip1.PerformLayout()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -118,4 +269,25 @@ Partial Class MainForm
     Friend WithEvents InserirToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents AjudaToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents DeathNoteEditor As RichTextBox
+    Friend WithEvents AbrirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalvarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents FontDialog1 As FontDialog
+    Friend WithEvents FonteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CorDoTextoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TemaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClaroToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EscuroToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MarkdownToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TítuloToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HTMLToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ParágrafoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents OpenFileName As ToolStripStatusLabel
+    Friend WithEvents WordCounter As ToolStripStatusLabel
+    Friend WithEvents SobreODeathnoteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NotasDeVersãoToolStripMenuItem As ToolStripMenuItem
 End Class
--- Deathnote/MainForm.vb
+++ Deathnote/MainForm.vb
@@ -1,6 +1,8 @@
 ﻿Imports System.IO
 
 Public Class MainForm
+    Dim fileTypes As String = ""
+
     Private Sub UpdateText(text As String)
         DeathNoteEditor.Text = text
     End Sub
@@ -11,5 +13,70 @@ Public Class MainForm
     End Sub
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LoadWelcomeText()
+    End Sub
+
+    Private Sub SaveFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles SaveFileDialog1.FileOk
+
+    End Sub
+
+    Private Sub AbrirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AbrirToolStripMenuItem.Click
+        Dim fileReader As System.IO.StreamReader
+
+        'OpenFileDialog1.Filter = "Arquivo Deathnote (*.dth^)|*.txt^"'
+        OpenFileDialog1.CheckFileExists = True
+        OpenFileDialog1.Title = "Escolha um arquivo"
+
+        If Me.OpenFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            Try
+                OpenFileDialog1.OpenFile()
+                fileReader = System.IO.File.OpenText(OpenFileDialog1.FileName)
+                DeathNoteEditor.Text = fileReader.ReadToEnd()
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        Else
+        End If
+    End Sub
+
+    Private Sub SalvarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalvarToolStripMenuItem.Click
+        'SaveFileDialog1.Filter = "Arquivo Deathnote (*.dth^)|*.dth^"'
+
+        If SaveFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, DeathNoteEditor.Text, FileAccess.Write)
+        End If
+    End Sub
+
+    Private Sub FonteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FonteToolStripMenuItem.Click
+        FontDialog1.ShowDialog()
+
+        DeathNoteEditor.Font = FontDialog1.Font()
+    End Sub
+
+    Private Sub CorDoTextoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CorDoTextoToolStripMenuItem.Click
+        ColorDialog1.ShowDialog()
+
+        DeathNoteEditor.ForeColor = ColorDialog1.Color()
+    End Sub
+
+    Private Sub UpdateWordCount(text As String)
+        Static rex As New System.Text.RegularExpressions.Regex("\b", System.Text.RegularExpressions.RegexOptions.Compiled Or System.Text.RegularExpressions.RegexOptions.Multiline)
+        Dim wordcount As Integer = (rex.Matches(DeathNoteEditor.Text).Count / 2)
+        Dim str As String
+
+        If Not wordcount = 1 Then
+            str = "palavras"
+        Else
+            str = "palavra"
+        End If
+
+        WordCounter.Text = wordcount.ToString() + str
+    End Sub
+
+    Private Sub DeathNoteEditor_TextChanged(sender As Object, e As EventArgs) Handles DeathNoteEditor.TextChanged
+        UpdateWordCount(DeathNoteEditor.Text)
+    End Sub
+
+    Private Sub AjudaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AjudaToolStripMenuItem.Click
+        MsgBox("aaa", MsgBoxStyle.Information, "Sobre o Deathnote")
     End Sub
 End Class
